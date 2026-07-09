d1f1f8364232969245f0e40ea69b7f2bdd4cb4c5
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-09 12:08:46 +0300

    Test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -7,6 +7,8 @@ describe('Gilded Rose', function () {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].name).to.equal('fixme');
+
+
     });
 
 });
