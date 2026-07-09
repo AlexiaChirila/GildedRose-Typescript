be59f4b96d4270610edbcfcca24f38934db8afdd
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-09 14:39:08 +0300

    Restyled the code for better view

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,38 +17,42 @@ export class GildedRose {
         this.items = items;
     }
 
-    private increaseQuality(item) {
+    private increaseQuality(item: Item) {
         if (item.quality < 50) {
             item.quality++;
         }
     }
 
-    private decreaseQuality(item) {
+    private decreaseQuality(item:Item) {
         if (item.quality > 0) {
             item.quality--;
         }
     }
 
-    private decreaseSellIn(item) {
+    private decreaseSellIn(item:Item) {
         item.sellIn--;
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-
+        for (let i = 0; i < this.items.length; i++)
+        {
             if (this.items[i].name === 'Sulfuras, Hand of Ragnaros')
                 continue;
+
             switch (this.items[i].name) {
+
                 case 'Aged Brie':
                     this.increaseQuality(this.items[i]);
                     if (this.items[i].sellIn <= 0) this.increaseQuality(this.items[i]);
                     break;
+
                 case 'Backstage passes to a TAFKAL80ETC concert':
                     this.increaseQuality(this.items[i]);
                     if (this.items[i].sellIn <= 10) this.increaseQuality(this.items[i]);
                     if (this.items[i].sellIn <= 5) this.increaseQuality(this.items[i]);
                     if (this.items[i].sellIn <= 0) this.items[i].quality = 0;
                     break;
+
                 case 'Conjured Mana Cake':
                     this.decreaseQuality(this.items[i]);
                     this.decreaseQuality(this.items[i]);
@@ -58,6 +62,7 @@ export class GildedRose {
                         this.decreaseQuality(this.items[i]);
                     }
                     break;
+
                 default:
                     this.decreaseQuality(this.items[i]);
                     if (this.items[i].sellIn <= 0) this.decreaseQuality(this.items[i]);
